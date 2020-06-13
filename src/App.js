e2619063acfb92e526d25d7a4d945d171c1dcce2
Marta <marta.marta.k@gmail.com>
2020-06-13 21:38:14 +0200

    chacking if config settings worked

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Person from './Person/Person';
 function App() {
   return (
     <div className="App">
+      <h1>MEET OUT TEAM</h1>
     <Person img="34" name="Helena" occupation="Front-end developer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. </Person>  
     <Person img="16" name="Brigitte" occupation="Back-end developer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. </Person> 
     <Person img="2" name="Cassandra" occupation="UX/UI designer">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt. </Person>   </div>
