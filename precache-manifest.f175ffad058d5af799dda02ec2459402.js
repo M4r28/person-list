dba1ab092c3a76b2e8d7ad388b16fc7f61fb8dd9
Marta <marta.kolodziejczak@quirion.de>
2020-06-13 20:58:43 +0200

    Updates

---
+++ precache-manifest.f175ffad058d5af799dda02ec2459402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e879ef1e609394b3973af66b5d5d6996",
+    "url": "/repo_name/index.html"
+  },
+  {
+    "revision": "ea608172821e98ee7786",
+    "url": "/repo_name/static/css/main.3c3d9257.chunk.css"
+  },
+  {
+    "revision": "880d7913ec6e61b4d3f5",
+    "url": "/repo_name/static/js/2.52e60a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/repo_name/static/js/2.52e60a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea608172821e98ee7786",
+    "url": "/repo_name/static/js/main.0882d2d2.chunk.js"
+  },
+  {
+    "revision": "cea16cd38fba1152147a",
+    "url": "/repo_name/static/js/runtime-main.2e0924a7.js"
+  }
+]);
