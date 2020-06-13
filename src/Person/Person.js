da1505a2327c79164233a1f0720f1ff5a046d8a7
Marta <marta.kolodziejczak@quirion.de>
2020-06-13 18:10:37 +0200

    url to const

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Person = (props)=>{
   // console.log(props);
+  const url = `https://randomuser.me/api/portraits/thumb/women/${props.img}.jpg`
   
 return (
   <article className="Person">
-    <img src={`https://randomuser.me/api/portraits/thumb/women/${props.img}.jpg`} alt="our staff member"/>
+    <img src={url} alt="our staff member"/>
     <h3>Name: {props.name}</h3>
     <h4>Occupation: {props.occupation}</h4>
     <p>{props.children}</p>
